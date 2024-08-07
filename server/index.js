e49d5c29fc25744bc1e4957126e69d9caf3938a4
Vijay Maurya <126171677+vjzest@users.noreply.github.com>
2024-08-07 17:23:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const userRoutes = require("./routes/User");
+const userRoutes = require("./routes/user");
 const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
 const courseRoutes = require("./routes/Course");
