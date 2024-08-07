fa225f1906f29d9568ffa37de6e9c6eeca6e1592
vjzest <vjzest9569@gmail.com>
2024-08-07 17:04:28 +0530

    first commit

---
+++ server/utils/mailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+async function mailSender(to, subject, html) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465, // Try 465 for SSL
+    secure: true, // Set to true for port 465
+    auth: {
+      user: "vjzest9569@gmail.com",
+      pass: "awgxmcjlonuvriyk", // Use an App Password
+    },
+    tls: {
+      rejectUnauthorized: false, // Allow self-signed certificates
+    },
+  });
+
+  const mailOptions = {
+    from: "vjzest9569@gmail.com",
+    to,
+    subject,
+    html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", info.response);
+    return info;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw new Error("Failed to send email");
+  }
+}
+
+module.exports = mailSender;
